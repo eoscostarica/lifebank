a8720c43eb37be5a1d95ea0265e6e3436cfdc277
Ruben <rubenabix@gmail.com>
2020-05-16 22:58:29 -0600

    fix: index table

--- contracts/consent2life/src/consent2life.cpp
+++ contracts/consent2life/src/consent2life.cpp
@@ -1,60 +1,72 @@
 #include <consent2life.hpp>
 
-ACTION consent2life::consent(name user, name contract, checksum256 hash) {
+ACTION consent2life::consent(name user, name contract, checksum256 hash)
+{
   require_auth(user);
 
   // Init the _records table
   informed_consent_table _records(get_self(), get_self().value);
-        // calculate unique record  
+  // calculate unique record
   auto single_record = (static_cast<uint128_t>(user.value) << 64) | contract.value;
-        // Find the record user _records table
+  // Find the record user _records table
   auto single_record_index = _records.get_index<name("singlerecord")>();
   auto existing_record = single_record_index.find(single_record);
 
-  if (existing_record == single_record_index.end()) {
+  if (existing_record == single_record_index.end())
+  {
     // Create a consent record if it does not exist
-    _records.emplace(get_self(), [&](auto& row) {
+    _records.emplace(get_self(), [&](auto &row) {
+      row.id = _records.available_primary_key();
       row.record = single_record;
       row.user = user;
       row.contract = contract;
       row.hash = hash;
     });
-  } else {
+  }
+  else
+  {
     // Modify a consent record if it exists
-    single_record_index.modify(existing_record, get_self(), [&](auto& row) {
+    single_record_index.modify(existing_record, get_self(), [&](auto &row) {
       row.hash = hash;
     });
   }
 }
 
-ACTION consent2life::revoke(name user, name contract) {
+ACTION consent2life::revoke(name user, name contract)
+{
   require_auth(user);
 
   informed_consent_table _records(get_self(), get_self().value);
 
-  // calculate unique record  
+  // calculate unique record
   auto single_record = (static_cast<uint128_t>(user.value) << 64) | contract.value;
 
   // Find and delete existing record in _records table
   auto itr = _records.begin();
-  while ( itr != _records.end()) {
-      if(itr->record == single_record){
-          itr = _records.erase(itr);
-          return;
-      }else{
-          itr++;
-      }
+  while (itr != _records.end())
+  {
+    if (itr->record == single_record)
+    {
+      itr = _records.erase(itr);
+      return;
+    }
+    else
+    {
+      itr++;
+    }
   }
 }
 
-ACTION consent2life::clear() {
+ACTION consent2life::clear()
+{
   require_auth(get_self());
 
   informed_consent_table _records(get_self(), get_self().value);
 
   // Delete all records in _records table
   auto consent_itr = _records.begin();
-  while (consent_itr != _records.end()) {
+  while (consent_itr != _records.end())
+  {
     consent_itr = _records.erase(consent_itr);
   }
 }
