ee301a6f28e7755e9b0c9a7f58601ce348591082
Ruben <rubenabix@gmail.com>
2020-05-19 18:03:43 -0600

    add tx id

--- contracts/lifebankcode/include/lifebankcode.hpp
+++ contracts/lifebankcode/include/lifebankcode.hpp
@@ -1,53 +1,84 @@
 #include <eosio/eosio.hpp>
+#include <eosio/transaction.hpp>
 
 using namespace std;
 using namespace eosio;
 
-CONTRACT lifebankcode : public contract {
-  public:
-    using contract::contract;
+CONTRACT lifebankcode : public contract
+{
+public:
+  using contract::contract;
 
-    ACTION adddoner(name account, string fullname);
-    ACTION clear();
+  ACTION adddoner(name account, string fullname);
+  ACTION addclinic(name account, string fullname);
+  ACTION addsponsor(name account, string fullname);
+  ACTION clear();
 
-  private:
+private:
+  void check_consent(name account);
+  checksum256 get_tx();
 
-    void check_consent(name account);
+  TABLE doner
+  {
+    name account;
+    string fullname;
+    checksum256 tx;
+    auto primary_key() const { return account.value; }
+    EOSLIB_SERIALIZE(doner,
+                     (account)(fullname)(tx));
+  };
+  typedef multi_index<name("doners"), doner> doner_table;
 
-    TABLE doner {
-      name     account;
-      string   fullname;
-      auto primary_key() const { return account.value; }
-    };
-    typedef multi_index<name("doners"), doner> doner_table;
+  TABLE clinic
+  {
+    name account;
+    checksum256 tx;
+    auto primary_key() const { return account.value; }
+    EOSLIB_SERIALIZE(clinic,
+                     (account)(tx));
+  };
+  typedef multi_index<name("clinics"), clinic> clinic_table;
+
+  TABLE sponsor
+  {
+    name account;
+    checksum256 tx;
+    auto primary_key() const { return account.value; }
+    EOSLIB_SERIALIZE(sponsor,
+                     (account)(tx));
+  };
+  typedef multi_index<name("sponsors"), sponsor> sponsor_table;
 };
 
 constexpr name consent_account{"consent2life"_n};
 
-struct informed_consent {
-  uint64_t     id;
-  uint128_t    record;
-  name         user;
-  name         contract;
-  checksum256  hash;
+struct informed_consent
+{
+  uint64_t id;
+  uint128_t record;
+  name user;
+  name contract;
+  checksum256 hash;
   EOSLIB_SERIALIZE(informed_consent,
-    (id)(record)(user)(contract)(hash));
+                   (id)(record)(user)(contract)(hash));
   auto primary_key() const { return id; }
   uint128_t get_record() const { return record; }
   checksum256 get_hash() const { return hash; }
 };
 
 typedef multi_index<name("userconsents"), informed_consent,
-  indexed_by<name("singlerecord"), const_mem_fun<informed_consent, uint128_t, &informed_consent::get_record>>,
-  indexed_by<name("byhash"), const_mem_fun<informed_consent, checksum256, &informed_consent::get_hash>>>
-  informed_consent_table;
+                    indexed_by<name("singlerecord"), const_mem_fun<informed_consent, uint128_t, &informed_consent::get_record>>,
+                    indexed_by<name("byhash"), const_mem_fun<informed_consent, checksum256, &informed_consent::get_hash>>>
+    informed_consent_table;
 
-bool has_consent(name account, name contract){
+bool has_consent(name account, name contract)
+{
   informed_consent_table _records(consent_account, consent_account.value);
   auto single_record = (static_cast<uint128_t>(account.value) << 64) | contract.value;
   auto single_record_index = _records.get_index<name("singlerecord")>();
   auto existing_record = single_record_index.find(single_record);
-  if (existing_record == single_record_index.end()) {
+  if (existing_record == single_record_index.end())
+  {
     return false;
   }
   return true;
--- contracts/lifebankcode/src/lifebankcode.cpp
+++ contracts/lifebankcode/src/lifebankcode.cpp
@@ -1,11 +1,22 @@
 #include <lifebankcode.hpp>
+#include <eosio/crypto.hpp>
 
-void lifebankcode::check_consent(name account) {
+checksum256 lifebankcode::get_tx()
+{
+  auto s = eosio::read_transaction(nullptr, 0);
+  char *tx = (char *)malloc(s);
+  read_transaction(tx, s);
+  return sha256(tx, s);
+}
+
+void lifebankcode::check_consent(name account)
+{
   bool consent = has_consent(account, get_self());
   eosio::check(consent, "Do not have consent for lifebankcode");
 }
 
-ACTION lifebankcode::adddoner(name account, string fullname) {
+ACTION lifebankcode::adddoner(name account, string fullname)
+{
   require_auth(account);
   check_consent(account);
   doner_table _doners(get_self(), get_self().value);
@@ -16,18 +27,40 @@ ACTION lifebankcode::adddoner(name account, string fullname) {
     _doners.emplace(get_self(), [&](auto &row) {
       row.account = account;
       row.fullname = fullname;
+      row.tx = get_tx();
     });
   }
   else
   {
     _doners.modify(doner_itr, get_self(), [&](auto &row) {
       row.fullname = fullname;
+      row.tx = get_tx();
     });
   }
 }
 
-ACTION lifebankcode::clear() {
+ACTION lifebankcode::addclinic(name account, string fullname)
+{
+  require_auth(account);
+}
+
+ACTION lifebankcode::addsponsor(name account, string fullname)
+{
+  require_auth(account);
+}
+
+ACTION lifebankcode::clear()
+{
+  // DEV only
   require_auth(get_self());
+
+  doner_table _doners(get_self(), get_self().value);
+
+  auto doners_itr = _doners.begin();
+  while (doners_itr != _doners.end())
+  {
+    doners_itr = _doners.erase(doners_itr);
+  }
 }
 
 EOSIO_DISPATCH(lifebankcode, (adddoner)(clear))
