777b4b7537a0440cd8196359077b23a97b14fcac
Ruben <rubenabix@gmail.com>
2020-05-26 22:24:41 -0600

    use sha256 hash

--- contracts/consent2life/include/consent2life.hpp
+++ contracts/consent2life/include/consent2life.hpp
@@ -1,4 +1,5 @@
 #include <eosio/eosio.hpp>
+#include <eosio/crypto.hpp>
 
 using namespace std;
 using namespace eosio;
@@ -13,21 +14,22 @@ public:
   ACTION clear();
 
 private:
+  static checksum256 string_to_hash(const string &input);
   TABLE informed_consent
   {
     uint64_t id;
-    uint128_t record;
+    checksum256 record;
     name user;
     name contract;
     checksum256 hash;
     EOSLIB_SERIALIZE(informed_consent,
                      (id)(record)(user)(contract)(hash));
     auto primary_key() const { return id; }
-    uint128_t get_record() const { return record; }
+    checksum256 get_record() const { return record; }
     checksum256 get_hash() const { return hash; }
   };
   typedef multi_index<name("userconsents"), informed_consent,
-                      indexed_by<name("singlerecord"), const_mem_fun<informed_consent, uint128_t, &informed_consent::get_record>>,
+                      indexed_by<name("singlerecord"), const_mem_fun<informed_consent, checksum256, &informed_consent::get_record>>,
                       indexed_by<name("byhash"), const_mem_fun<informed_consent, checksum256, &informed_consent::get_hash>>>
       informed_consent_table;
 };
