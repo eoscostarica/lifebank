18c57731c8e99dbd716f7be59aea2c3939fe5821
JustinCast <justincastillovalladares@gmail.com>
2020-09-01 13:37:04 -0600

    wip: update docker-compose

---
+++ webapp/src/gql/offer.gql.js
@@ -0,0 +1,29 @@
+import gql from 'graphql-tag'
+
+export const CREATE_OFFER_MUTATION = gql`
+  mutation(
+    $limited: Boolean!
+    $online_only: Boolean!
+    $quantity: Int!
+    $offer_type: String!
+    $description: String!
+    $start_date: String!
+    $end_date: String!
+    $images: String!
+    $sponsor_id: Int!
+  ) {
+    create_offer(
+      limited: $limited
+      online_only: $online_only
+      quantity: $quantity
+      offer_type: $offer_type
+      description: $description
+      start_date: $start_date
+      end_date: $end_date
+      images: $images
+      sponsor_id: $sponsor_id
+    ) {
+      id
+    }
+  }
+`
