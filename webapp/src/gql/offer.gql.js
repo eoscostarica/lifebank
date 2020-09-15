3df33f49add1741c3c140f00a6513c30b3504aae
JustinCast <justincastillovalladares@gmail.com>
2020-09-15 14:46:43 -0600

    feat: edit functionality now working

--- webapp/src/gql/offer.gql.js
+++ webapp/src/gql/offer.gql.js
@@ -54,6 +54,42 @@ export const UPDATE_OFFER_AVAILABILITY_MUTATION = gql`
   }
 `
 
+export const UPDATE_OFFER_MUTATION = gql`
+  mutation(
+    $limited: Boolean
+    $online_only: Boolean
+    $quantity: Int
+    $offer_type: String
+    $description: String
+    $start_date: String
+    $end_date: String
+    $images: String
+    $active: Boolean
+    $offer_name: String
+    $id: Int!
+  ) {
+    update_offer(
+      _set: {
+        limited: $limited
+        online_only: $online_only
+        quantity: $quantity
+        offer_type: $offer_type
+        description: $description
+        start_date: $start_date
+        end_date: $end_date
+        images: $images
+        active: $active
+        offer_name: $offer_name
+      }
+      where: { id: { _eq: $id } }
+    ) {
+      returning {
+        id
+      }
+    }
+  }
+`
+
 export const GET_SPONSOR_OFFERS_QUERY = gql`
   query($sponsor_id: Int!) {
     offer(where: { sponsor_id: { _eq: $sponsor_id } }) {
