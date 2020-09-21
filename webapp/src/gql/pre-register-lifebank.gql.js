18c8180eefa9a27593decde5ffb11bfd2a6facf1
AngeloCG97 <51149817+AngeloCG97@users.noreply.github.com>
2020-09-21 17:24:06 -0600

    fix struct code

--- webapp/src/gql/pre-register-lifebank.gql.js
+++ webapp/src/gql/pre-register-lifebank.gql.js
@@ -1,8 +1,32 @@
 import gql from 'graphql-tag'
 
 export const CREATE_PRE_REGITER_LIFEBANK_MUTATION = gql`
-  mutation($email: String!, $password: String!, $name: String!, $address: String!, $schedule: String!, $phone: String!, $description: String!, $urgency_level: Int!, $coordinates: String!, $immunity_test: Boolean!, $invitation_code: String!) {
-    create_pre_register_lifebank(email: $email, password: $password, name: $name, address: $address, schedule: $schedule, phone: $phone, description: $description, urgency_level: $urgency_level, coordinates: $coordinates, immunity_test: $immunity_test, invitation_code: $invitation_code) {
+  mutation(
+    $email: String!, 
+    $password: String!, 
+    $name: String!, 
+    $address: String!, 
+    $schedule: String!, 
+    $phone: String!, 
+    $description: String!, 
+    $urgency_level: Int!, 
+    $coordinates: String!, 
+    $immunity_test: Boolean!, 
+    $invitation_code: String!
+  ) {
+    create_pre_register_lifebank(
+      email: $email, 
+      password: $password, 
+      name: $name, 
+      address: $address, 
+      schedule: $schedule, 
+      phone: $phone, 
+      description: $description, 
+      urgency_level: $urgency_level, 
+      coordinates: $coordinates, 
+      immunity_test: $immunity_test, 
+      invitation_code: $invitation_code
+  ) {
       resultRegister
     }
   }
