5490383014a5b4eec5d8694ffb4452de5af7823b
AngeloCG97 <51149817+AngeloCG97@users.noreply.github.com>
2020-09-21 17:20:02 -0600

    Delete blank space

--- hapi/src/routes/pre-register/pre-register-lifebank.handler.js
+++ hapi/src/routes/pre-register/pre-register-lifebank.handler.js
@@ -6,7 +6,6 @@ const lifebankApi = require('../../api/lifebank.api')
 module.exports = async ({ payload: { input } }) => {
   try {
     const response = await lifebankApi.preRegister(input)
-
     return response
   } catch (error) {
     return Boom.boomify(error, { statusCode: INTERNAL_SERVER_ERROR })
