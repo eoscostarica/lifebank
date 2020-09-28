eaaab30c3bec401c649e680fffa34a770d488826
AngeloCG97 <angelocas13_8@hotmail.com>
2020-09-28 17:01:54 -0600

    fix codefactor warnings

--- hapi/src/utils/mail.js
+++ hapi/src/utils/mail.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer')
 const { mailConfig } = require('../config')
 
 const sendVerificationCode = async (to, verficationCode) => {
-  let transporter = nodemailer.createTransport({
+  const transporter = nodemailer.createTransport({
     host: mailConfig.host,
     secure: false,
     port: mailConfig.port,
@@ -19,14 +19,11 @@ const sendVerificationCode = async (to, verficationCode) => {
     to,
     subject: 'Lifebank Verfication Code',
     html: `
-          <head>
-          ​
+          <head>​
             <meta charset="utf-8">
             <meta http-equiv="x-ua-compatible" content="ie=edge">
             <title>Email verification</title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            
-          ​
             <style type="text/css">
             /**
              * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
@@ -36,16 +33,14 @@ const sendVerificationCode = async (to, verficationCode) => {
                 font-style: normal;
                 font-weight: 400;
                 src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
-              }
-          ​
+              }          ​
               @font-face {
                 font-family: 'Source Sans Pro';
                 font-style: normal;
                 font-weight: 700;
                 src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
               }
-            }
-          ​
+            }​
             /**
              * Avoid browser level font resizing.
              * 1. Windows Mobile
@@ -57,8 +52,7 @@ const sendVerificationCode = async (to, verficationCode) => {
             a {
               -ms-text-size-adjust: 100%; /* 1 */
               -webkit-text-size-adjust: 100%; /* 2 */
-            }
-          ​
+            }​
             /**
              * Remove extra space added to tables and cells in Outlook.
              */
@@ -66,15 +60,13 @@ const sendVerificationCode = async (to, verficationCode) => {
             td {
               mso-table-rspace: 0pt;
               mso-table-lspace: 0pt;
-            }
-          ​
+            }​
             /**
              * Better fluid images in Internet Explorer.
              */
             img {
               -ms-interpolation-mode: bicubic;
-            }
-          ​
+            }​
             /**
              * Remove blue links for iOS devices.
              */
@@ -85,33 +77,28 @@ const sendVerificationCode = async (to, verficationCode) => {
               line-height: inherit !important;
               color: inherit !important;
               text-decoration: none !important;
-            }
-          ​
+            }​
             /**
              * Fix centering issues in Android 4.4.
              */
             div[style*="margin: 16px 0;"] {
               margin: 0 !important;
-            }
-          ​
+            }​
             body {
               width: 100% !important;
               height: 100% !important;
               padding: 0 !important;
               margin: 0 !important;
-            }
-          ​
+            }​
             /**
              * Collapse table borders to avoid space between cells.
              */
             table {
               border-collapse: collapse !important;
             }
-          ​
             a {
               color: #738EFF;
             }
-          ​
             img {
               height: auto;
               line-height: 100%;
@@ -120,19 +107,15 @@ const sendVerificationCode = async (to, verficationCode) => {
               outline: none;
             }
             </style>
-          ​
           </head>
-          <body style="background-color: #f6f6f6;">
-          ​
+          <body style="background-color: #f6f6f6;">​
             <!-- start preheader -->
             <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
               Candidato al CI, Campus Tecnológicos y Centros Académicos.
             </div>
-            <!-- end preheader -->
-          ​
+            <!-- end preheader -->​
             <!-- start body -->
-            <table border="0" cellpadding="0" cellspacing="0" width="100%">
-          ​
+            <table border="0" cellpadding="0" cellspacing="0" width="100%">​
               <!-- start logo -->
               <tr>
                 <td align="center" bgcolor="#f6f6f6">
@@ -150,8 +133,7 @@ const sendVerificationCode = async (to, verficationCode) => {
               <tr>
                 <td align="center" bgcolor="#f6f6f6">
               
-                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-          ​
+                  <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">​
                     <tr>
                       <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #f6f6f6;">
                         <h1 style="margin: 0; font-size: 32px; font-weight: bold; line-height: 48px; color: #BA0D0D;">Email verification</h1>
@@ -177,29 +159,26 @@ const sendVerificationCode = async (to, verficationCode) => {
                           Verify
                         </a>
                       </td>
-                    </tr>                  ​
+                    </tr>​
                     <tr>
                       <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; text-align: justify; border-bottom: 3px solid #BA0D0D">
                       </td>
                     </tr>
-                  </table>
-          ​
+                  </table>​
                 </td>
               </tr>
               <tr>
                 <td align="center" bgcolor="#f6f6f6" style="padding: 24px;">                
                 </td>
-              </tr>
-          ​
-            </table>
-          ​
+              </tr>​
+            </table>​
           </body>
     `
   })
 }
 
 const sendCredentialsRecovery = async (to, { username, secret }) => {
-  let transporter = nodemailer.createTransport({
+  const transporter = nodemailer.createTransport({
     host: mailConfig.host,
     secure: false,
     port: mailConfig.port,
