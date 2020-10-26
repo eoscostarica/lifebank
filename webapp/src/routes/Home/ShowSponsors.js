e81797650a21065bcd009be7627e894bf56d1798
JustinCast <justincastillovalladares@gmail.com>
2020-10-26 16:19:01 -0600

    feat: add new files

--- webapp/src/routes/Home/ShowSponsors.js
+++ webapp/src/routes/Home/ShowSponsors.js
@@ -1,64 +1,64 @@
-
 import React from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/styles'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
-import CircularProgress from '@material-ui/core/CircularProgress';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import StorefrontIcon from '@material-ui/icons/Storefront';
+import CircularProgress from '@material-ui/core/CircularProgress'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
+import StorefrontIcon from '@material-ui/icons/Storefront'
 import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
+import { useTranslation } from 'react-i18next'
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    width: "100%",
+    width: '100%'
   },
   list: {
-    width: "100vw",
+    width: '100vw'
   },
   listItem: {
-    width: "100%",
-    backgroundColor: "white"
-  }
-  , secondaryIconList: {
-    color: "rgba(0, 0, 0, 0.6)",
+    width: '100%',
+    backgroundColor: 'white'
+  },
+  secondaryIconList: {
+    color: 'rgba(0, 0, 0, 0.6)',
     width: 20,
     height: 20
   },
   listItemPrimaryText: {
-    color: "rgba(0, 0, 0, 0.87)",
-    fontFamily: "Roboto",
-    fontSize: "16px",
-    fontWeight: "normal",
-    fontStretch: "normal",
-    fontStyle: "normal",
+    color: 'rgba(0, 0, 0, 0.87)',
+    fontFamily: 'Roboto',
+    fontSize: '16px',
+    fontWeight: 'normal',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
     lineHeight: 1.5,
-    letterSpacing: "0.15px",
+    letterSpacing: '0.15px'
   },
   listItemSecondaryText: {
-    color: "color: rgba(0, 0, 0, 0.6)",
-    fontFamily: "Roboto",
-    fontSize: "14px",
-    fontWeight: "normal",
-    fontStretch: "normal",
-    fontStyle: "normal",
+    color: 'color: rgba(0, 0, 0, 0.6)',
+    fontFamily: 'Roboto',
+    fontSize: '14px',
+    fontWeight: 'normal',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
     lineHeight: 1.43,
-    letterSpacing: "0.25px",
+    letterSpacing: '0.25px'
   },
   sponsorsGridContainer: {
-    overflow: "auto",
-    whiteSpace: "nowrap",
-    width: "100%",
+    overflow: 'auto',
+    whiteSpace: 'nowrap',
+    width: '100%',
     marginTop: 15,
     marginBottom: 15,
     paddingBottom: 5,
@@ -67,54 +67,54 @@ const useStyles = makeStyles((theme) => ({
       height: '0.5em'
     },
     '&::-webkit-scrollbar-track': {
-      backgroundColor: 'rgba(0,0,0,.05)',
+      backgroundColor: 'rgba(0,0,0,.05)'
     },
     '&::-webkit-scrollbar-thumb': {
       backgroundColor: 'rgba(0,0,0,.1)',
-      borderRadius: "10px",
+      borderRadius: '10px'
     }
   },
   cardRoot: {
-    whiteSpace: "normal",
-    display: "inline-block",
-    position: "relative",
-    width: "265px",
-    height: "145px",
+    whiteSpace: 'normal',
+    display: 'inline-block',
+    position: 'relative',
+    width: '265px',
+    height: '145px',
     padding: 10,
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(2)
   },
   cardHeader: {
     padding: 0,
     margin: 0,
-    width: "100%",
-    display: "flex",
+    width: '100%',
+    display: 'flex',
     flexDirection: 'row',
-    alignItems: 'flex-start',
+    alignItems: 'flex-start'
   },
   cardAvatar: {
-    height: "40px",
-    width: "40px",
+    height: '40px',
+    width: '40px'
   },
   cardTitleContainer: {
-    width: "60%"
+    width: '60%'
   },
   cardTitle: {
     marginLeft: 7,
     marginTop: 10,
-    width: "100%",
-    fontFamily: "Roboto",
-    fontsize: "16px",
-    fontweight: "normal",
-    fontStretch: "normal",
-    fontStyle: "normal",
-    lineHeight: "1.5",
-    letterSpacing: "0.15px",
-    textAlign: "left",
-    color: "rgba(0, 0, 0, 0.87)",
+    width: '100%',
+    fontFamily: 'Roboto',
+    fontsize: '16px',
+    fontweight: 'normal',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: '1.5',
+    letterSpacing: '0.15px',
+    textAlign: 'left',
+    color: 'rgba(0, 0, 0, 0.87)'
   },
   cardIconOffer: {
-    position: "absolute",
-    color: "rgba(0, 0, 0, 0.6)",
+    position: 'absolute',
+    color: 'rgba(0, 0, 0, 0.6)',
     width: 20,
     height: 20,
     top: 20,
@@ -122,70 +122,79 @@ const useStyles = makeStyles((theme) => ({
   },
   cardContent: {
     marginTop: 10,
-    padding: 0,
+    padding: 0
   },
   cardContentText: {
-    fontFamily: "Roboto",
-    fontsize: "14px",
-    fontweight: "normal",
-    fontStretch: "normal",
-    fontStyle: "normal",
-    lineHeight: "1.43",
-    letterSpacing: "0.25px",
-    textAlign: "left",
-    color: "rgba(0, 0, 0, 0.6)",
+    fontFamily: 'Roboto',
+    fontsize: '14px',
+    fontweight: 'normal',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: '1.43',
+    letterSpacing: '0.25px',
+    textAlign: 'left',
+    color: 'rgba(0, 0, 0, 0.6)'
   },
   cardActions: {
     padding: 0,
-    width: "100%",
-    display: "flex",
+    width: '100%',
+    display: 'flex',
     flexDirection: 'column',
-    alignItems: 'flex-end',
+    alignItems: 'flex-end'
   },
   cardActionButton: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 10,
     right: 15,
-    fontFamily: "Roboto",
-    fontsize: "14px",
-    fontweight: "normal",
-    fontStretch: "normal",
-    fontStyle: "normal",
-    lineHeight: "1.14",
-    letterSpacing: "1",
-    textAlign: "center",
-    color: "#121212",
+    fontFamily: 'Roboto',
+    fontsize: '14px',
+    fontweight: 'normal',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: '1.14',
+    letterSpacing: '1',
+    textAlign: 'center',
+    color: '#121212'
   }
 }))
 
 const ShowSponsors = ({ sponsors, loading, isDesktop }) => {
+  const { t } = useTranslation('translations')
   const classes = useStyles()
 
   const LoadSponsors = () => {
     return (
       <>
-        {loading &&
+        {loading && (
           <Box className={classes.wrapper}>
             <CircularProgress />
           </Box>
-        }
+        )}
         {!loading && sponsors.length <= 0 && (
-          <ListItem className={classes.listItem} >
+          <ListItem className={classes.listItem}>
             <ListItemText
               primary={
-                <Typography className={classes.listItemPrimaryText} noWrap variant="body2">No sponsors available</Typography>
+                <Typography
+                  className={classes.listItemPrimaryText}
+                  noWrap
+                  variant="body2"
+                >
+                  {t('miscellaneous.noSponsorsAvailable')}
+                </Typography>
               }
             />
           </ListItem>
         )}
-        {!loading && sponsors.length > 0 && sponsors.map(sponsor => (
-          <SponsorItem
-            key={sponsor.id}
-            id={sponsor.id}
-            name={sponsor.name}
-            bussines_type={sponsor.info.bussines_type}
-          />
-        ))}
+        {!loading &&
+          sponsors.length > 0 &&
+          sponsors.map((sponsor) => (
+            <SponsorItem
+              key={sponsor.id}
+              id={sponsor.id}
+              name={sponsor.name}
+              bussines_type={sponsor.info.bussines_type}
+            />
+          ))}
       </>
     )
   }
@@ -200,10 +209,22 @@ const ShowSponsors = ({ sponsors, loading, isDesktop }) => {
         </ListItemAvatar>
         <ListItemText
           primary={
-            <Typography className={classes.listItemPrimaryText} noWrap variant="body2">{props.name}</Typography>
+            <Typography
+              className={classes.listItemPrimaryText}
+              noWrap
+              variant="body2"
+            >
+              {props.name}
+            </Typography>
           }
           secondary={
-            <Typography className={classes.listItemSecondaryText} noWrap variant="body2">{props.bussines_type}</Typography>
+            <Typography
+              className={classes.listItemSecondaryText}
+              noWrap
+              variant="body2"
+            >
+              {props.bussines_type}
+            </Typography>
           }
         />
       </ListItem>
@@ -212,17 +233,17 @@ const ShowSponsors = ({ sponsors, loading, isDesktop }) => {
 
   SponsorItem.propTypes = {
     name: PropTypes.string,
-    bussines_type: PropTypes.string,
+    bussines_type: PropTypes.string
   }
 
   const LoadSponsorsDesktop = () => {
     return (
       <>
-        {loading &&
+        {loading && (
           <Box className={classes.wrapper}>
             <CircularProgress />
           </Box>
-        }
+        )}
         {!loading && sponsors.length <= 0 && (
           <Card className={classes.cardRoot}>
             <Grid
@@ -230,22 +251,28 @@ const ShowSponsors = ({ sponsors, loading, isDesktop }) => {
               direction="row"
               justify="center"
               alignItems="center"
-              style={{ height: "100%" }}
+              style={{ height: '100%' }}
             >
-              <Typography className={classes.title} color="textSecondary" gutterBottom>
-                No sponsors available
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                {t('miscellaneous.noSponsorsAvailable')}
               </Typography>
             </Grid>
           </Card>
         )}
-        {!loading && sponsors.length > 0 && sponsors.map(sponsor => (
-          <SponsorCard
-            key={sponsor.id}
-            id={sponsor.id}
-            name={sponsor.name}
-            description={sponsor.info.benefit_description}
-          />
-        ))}
+        {!loading &&
+          sponsors.length > 0 &&
+          sponsors.map((sponsor) => (
+            <SponsorCard
+              key={sponsor.id}
+              id={sponsor.id}
+              name={sponsor.name}
+              description={sponsor.info.benefit_description}
+            />
+          ))}
       </>
     )
   }
@@ -253,8 +280,7 @@ const ShowSponsors = ({ sponsors, loading, isDesktop }) => {
   const truncateString = (str) => {
     const num = 55
 
-    if (str.length <= num)
-      return str
+    if (str.length <= num) return str
 
     return str.slice(0, num) + '...'
   }
@@ -263,7 +289,7 @@ const ShowSponsors = ({ sponsors, loading, isDesktop }) => {
     return (
       <Card className={classes.cardRoot}>
         <Box className={classes.cardHeader}>
-          <Avatar className={classes.cardAvatar} >
+          <Avatar className={classes.cardAvatar}>
             <StorefrontIcon />
           </Avatar>
           <Box className={classes.cardTitleContainer}>
@@ -273,11 +299,12 @@ const ShowSponsors = ({ sponsors, loading, isDesktop }) => {
           </Box>
         </Box>
         <CardContent className={classes.cardContent}>
-          <Typography className={classes.cardContentText} >{truncateString(props.description)}
+          <Typography className={classes.cardContentText}>
+            {truncateString(props.description)}
           </Typography>
         </CardContent>
         <Button color="primary" className={classes.cardActionButton}>
-          more info
+          {t('cardsSection.moreInfo')}
         </Button>
       </Card>
     )
@@ -285,21 +312,21 @@ const ShowSponsors = ({ sponsors, loading, isDesktop }) => {
 
   SponsorCard.propTypes = {
     name: PropTypes.string,
-    description: PropTypes.string,
+    description: PropTypes.string
   }
 
   return (
     <>
-      {!isDesktop &&
-        <List className={classes.list} >
+      {!isDesktop && (
+        <List className={classes.list}>
           <LoadSponsors />
         </List>
-      }
-      {isDesktop &&
+      )}
+      {isDesktop && (
         <Box className={classes.sponsorsGridContainer}>
           <LoadSponsorsDesktop />
         </Box>
-      }
+      )}
     </>
   )
 }
@@ -307,7 +334,7 @@ const ShowSponsors = ({ sponsors, loading, isDesktop }) => {
 ShowSponsors.propTypes = {
   sponsors: PropTypes.array,
   loading: PropTypes.bool,
-  isDesktop: PropTypes.bool,
+  isDesktop: PropTypes.bool
 }
 
 export default ShowSponsors
