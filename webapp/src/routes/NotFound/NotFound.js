6d628ef8023956d5499995d2e2aae398e6056734
JeanVegaD <jean0798@gmail.com>
2020-11-11 09:52:58 -0600

    refactor: improve style in not found component

--- webapp/src/routes/NotFound/NotFound.js
+++ webapp/src/routes/NotFound/NotFound.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(4),
     display: 'flex',
-    height: '100vh'
+    height: 'calc(100vh - 60px)'
   },
   content: {
     justifyContent: 'center',
@@ -19,8 +19,49 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex'
   },
   centerText: {
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
+  tittle: {
+    fontFamily: "Roboto",
+    fontSize: "34px",
+    fontWeight: "normal",
+    fontStretch: "normal",
+    fontStyle: "normal",
+    lineHeight: "1.18",
+    letterSpacing: "0.25px",
+    textAlign: "center",
+    color: "rgba(0, 0, 0, 0.87)",
+    marginBottom: 15
+  },
+  subTitle: {
+    fontFamily: "Roboto",
+    fontSize: "14px",
+    fontWeight: "normal",
+    fontStretch: "normal",
+    fontStyle: "normal",
+    lineHeight: "1.43",
+    letterSpacing: "0.25px",
+    textAlign: "justify",
+    color: "rgba(0, 0, 0, 0.6)",
+    marginBottom: 30
+  },
+  btnHome: {
+    borderRadius: '50px',
+    backgroundColor: '#ba0d0d',
+    width: "50%",
+    fontSize: '14px',
+    fontWeight: 500,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: 1.14,
+    letterSpacing: '1px',
+    color: '#ffffff',
+    padding: '12px',
+    marginBottom: 10,
+    [theme.breakpoints.down('md')]: {
+      width: "100%",
+    }
+  },
 }))
 
 const NotFound = () => {
@@ -32,18 +73,16 @@ const NotFound = () => {
       <Grid container spacing={4}>
         <Grid item xs={12} className={classes.content}>
           <div className={classes.centerText}>
-            <Typography variant="h1">{t('notFound.404Error')}</Typography>
-            <Typography variant="subtitle2">
-              {t('notFound.notFoundMessage')}
-            </Typography>
+            <Typography className={classes.tittle}>{t('notFound.404Error')}</Typography>
+            <Typography className={classes.subTitle}>{t('notFound.notFoundMessage')}</Typography>
             <Button
               variant="contained"
-              color="primary"
-              activeClassName={classes.active}
+              color="secondary"
+              className={classes.btnHome}
               component={CustomRouterLink}
               to="/"
             >
-              {t('notFound.TakeMeHome')}
+              {t('notFound.takeMeHome')}
             </Button>
           </div>
         </Grid>
