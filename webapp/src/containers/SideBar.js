a9b0fae766bbfc6bcc3dd24500c4fd4487e46b27
Edgar097 <edgar.parra@eoscostarica.io>
2021-03-08 17:05:47 -0500

    remove unused useEffect

--- webapp/src/containers/SideBar.js
+++ webapp/src/containers/SideBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/styles'
 import Typography from '@material-ui/core/Typography'
