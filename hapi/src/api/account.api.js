1c432a3b3c6b2794cc86889e2fc438b0a8bf09a9
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2021-03-29 05:40:36 -0600

    fix(backend): remove login vulnerabilities

--- hapi/src/api/account.api.js
+++ hapi/src/api/account.api.js
@@ -435,17 +435,23 @@ const verifyEmail = async ({ code }) => {
   }
 }
 
-const login = async ({ account, secret }) => {
+const login = async ({ account, password }) => {
+  const bcrypt = require('bcryptjs')
   const user = await userApi.getOne({
     _or: [
       { email: { _eq: account } },
       { username: { _eq: account } },
       { account: { _eq: account } }
-    ],
-    secret: { _eq: secret }
+    ]
   })
 
   if (!user) {
+    throw new Error('Invalid account or secret')
+  }
+
+  const comparison = await bcrypt.compare(password, user.secret)
+
+  if (!comparison) {
     throw new Error('Invalid account or secret')
   }
 
