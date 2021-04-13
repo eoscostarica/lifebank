e8daee67d5990f2891a3010abd4ecb8fbbd0d458
Edgar097 <edgar.parra@eoscostarica.io>
2021-04-13 14:11:47 -0600

    fix(email bug): remove email from memory

--- webapp/src/components/Signup/Signup.js
+++ webapp/src/components/Signup/Signup.js
@@ -434,6 +434,13 @@ const Signup = ({ isHome, isModal, isSideBar }) => {
 
   }, [errorpreRegisterLifebank])
 
+  useEffect(()=>{
+    if(open){
+      handleSetField('email', ' ')
+      setActiveStep(0)
+    }
+    },[open])
+
   const ErrorMessage = () => {
     return (
       <>
