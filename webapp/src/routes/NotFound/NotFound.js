c18d1daff448842716526e30a5a076c44b98d14b
Edgar097 <edgar.parra@eoscostarica.io>
2021-04-15 17:06:51 -0600

    fix(styles): move styles to their own file

--- webapp/src/routes/NotFound/NotFound.js
+++ webapp/src/routes/NotFound/NotFound.js
@@ -8,71 +8,9 @@ import { Box } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 
 import CustomRouterLink from '../../components/CustomRouterLink'
+import styles from './styles'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(4),
-    display: 'flex',
-    height: 'calc(100vh - 60px)'
-  },
-  content: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    display: 'flex'
-  },
-  centerText: {
-    textAlign: 'center',
-    paddingLeft: "5%",
-    paddingRight: "5%"
-  },
-  tittle: {
-    fontFamily: "Roboto",
-    fontSize: "34px",
-    fontWeight: "normal",
-    fontStretch: "normal",
-    fontStyle: "normal",
-    lineHeight: "1.18",
-    letterSpacing: "0.25px",
-    textAlign: "center",
-    color: "rgba(0, 0, 0, 0.87)",
-    marginBottom: 15,
-    textDecoration: 'none'
-  },
-  subTitle: {
-    fontFamily: "Roboto",
-    fontSize: "14px",
-    fontWeight: "normal",
-    fontStretch: "normal",
-    fontStyle: "normal",
-    lineHeight: "1.43",
-    letterSpacing: "0.25px",
-    textAlign: "center",
-    color: "rgba(0, 0, 0, 0.6)",
-    marginBottom: 30,
-    marginLeft: "10%",
-    marginRight: "10%"
-  },
-  link: {
-    textDecoration: 'none'
-  },
-  btnHome: {
-    borderRadius: '50px',
-    backgroundColor: '#ba0d0d',
-    width: "25%",
-    fontSize: '14px',
-    fontWeight: 500,
-    fontStretch: 'normal',
-    fontStyle: 'normal',
-    lineHeight: 1.14,
-    letterSpacing: '1px',
-    color: '#ffffff',
-    padding: '12px',
-    marginBottom: 10,
-    [theme.breakpoints.down('sm')]: {
-      width: "60%",
-    }
-  },
-}))
+const useStyles = makeStyles(styles)
 
 const NotFound = () => {
   const { t } = useTranslation('translations')
--- webapp/src/routes/OffersManagement/OffersManagement.js
+++ webapp/src/routes/OffersManagement/OffersManagement.js
@@ -32,68 +32,9 @@ import {
 
 import OfferDetails from './OfferDetails'
 import GenericOfferFormComponent from './GenericOfferFormComponent'
+import styles from './styles'
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(4),
-    display: 'flex',
-    height: 'calc(100vh - 60px)',
-    width: "90%",
-    [theme.breakpoints.down('md')]: {
-      width: "100%",
-    }
-  },
-  title: {
-    fontSize: "34px",
-    fontWeight: "normal",
-    fontStretch: "normal",
-    fontStyle: "normal",
-    lineHeight: "1.18",
-    letterSpacing: "0.25px",
-    textAlign: "center",
-    color: "rgba(0, 0, 0, 0.87)",
-    marginBottom: 15
-  },
-  btnAction: {
-    borderRadius: '50px',
-    backgroundColor: '#ba0d0d',
-    width: "50%",
-    fontSize: '14px',
-    fontWeight: 500,
-    fontStretch: 'normal',
-    fontStyle: 'normal',
-    lineHeight: 1.14,
-    letterSpacing: '1px',
-    color: '#ffffff',
-    padding: '12px',
-    marginBottom: 10,
-    [theme.breakpoints.down('md')]: {
-      width: "100%",
-    }
-  },
-  content: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    display: 'flex',
-    width: "100%"
-  },
-  fab: {
-    position: 'fixed',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-    color: 'white'
-  },
-  formControl: {
-    width: '100%'
-  },
-  tableContent: {
-    paddingTop: "30px",
-    width: "100%"
-  }
-
-}))
+const useStyles = makeStyles(styles)
 
 const OffersManagement = () => {
   const { t } = useTranslation('translations')
