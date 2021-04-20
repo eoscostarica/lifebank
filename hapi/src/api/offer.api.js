1576755565a83f7f227023b83bafc97fdbf87dd6
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2021-04-20 09:37:33 -0600

    feat(backend): create redeem action

---
+++ hapi/src/api/offer.api.js
@@ -0,0 +1,5 @@
+// FROM liffivjjbcki
+// DETAILS { memo: 'Thanks for your donation', quantity: 1, to: 'donpiub3hlno' }
+const redeemOffer = async (from, details, offer) => {
+
+}
---
+++ hapi/src/routes/redeem-offer/redeem-offer.validation.js
@@ -0,0 +1,14 @@
+const Joi = require('@hapi/joi')
+
+module.exports = {
+  payload: Joi.object({
+    input: Joi.object({
+      role: Joi.string().required(),
+      email: Joi.string().required(),
+      emailContent: Joi.object().required(),
+      name: Joi.string().required(),
+      passwordPlainText: Joi.string().required(),
+      signup_method: Joi.string().required()
+    })
+  }).options({ stripUnknown: true })
+}
