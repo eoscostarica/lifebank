7eb4ee1172f64fd12533cc6be5d1164e2a860735
Edgar097 <edgar.parra@eoscostarica.io>
2021-04-26 14:03:27 -0600

    fix(create and push): docker images

--- utils/meta.mk
+++ utils/meta.mk
@@ -1,11 +1,17 @@
 -include .env
 
 VERSION=$(shell git rev-parse --short HEAD)
-IMAGE_NAME=""
-DOCKER_REGISTRY=""
+CURRENT_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD)
+
+IMAGE_NAME_WEBAPP=lifebank-webapp
+IMAGE_NAME_HAPI=lifebank-hapi
+IMAGE_NAME_HASURA=lifebank-hasura
+IMAGE_NAME_WALLET=lifebank-wallet
+
+DOCKER_REGISTRY=edgar097
 SUBDIRS = webapp hapi hasura wallet
 
-MAKE_ENV += DOCKER_REGISTRY VERSION IMAGE_NAME
+MAKE_ENV += DOCKER_REGISTRY VERSION IMAGE_NAME_WEBAPP IMAGE_NAME_HAPI IMAGE_NAME_WALLET IMAGE_NAME_HASURA
 
 SHELL_EXPORT := $(foreach v,$(MAKE_ENV),$(v)='$($(v))')
 
