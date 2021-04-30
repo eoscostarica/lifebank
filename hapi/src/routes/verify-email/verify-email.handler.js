51bfc972bb722caac687b756ad83b3e2a52b03c4
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2021-04-30 08:38:59 -0600

    backend(feat): test

--- hapi/src/routes/verify-email/verify-email.handler.js
+++ hapi/src/routes/verify-email/verify-email.handler.js
@@ -5,10 +5,14 @@ const { accountApi } = require('../../api')
 
 module.exports = async ({ payload: { input } }) => {
   try {
+    console.log('INPUT', input)
+    console.log('ACCOUNT', accountApi)
     const response = await accountApi.verifyEmail(input)
+    console.log('PASS')
 
     return response
   } catch (error) {
+    console.log('ERROR', error)
     return Boom.boomify(error, { statusCode: INTERNAL_SERVER_ERROR })
   }
 }
