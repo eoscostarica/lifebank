e43d2db0d4a840adbb93da8f3510de7647a01473
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2021-05-07 10:42:42 -0600

    fix(frontend): move download report from topbar to sidebar

--- webapp/src/containers/SideBar.js
+++ webapp/src/containers/SideBar.js
@@ -132,7 +132,7 @@ const SideBar = ({ user, onLogout, triggerSideBarPosition }) => {
           </Typography>
           <Box className={classes.optionLink} onClick={triggerSideBarPosition}>
             <EditIcon className={classes.iconOption} />
-            <Link to="/terms-of-useaa">
+            <Link to="/">
               <Typography variant="body1" className={classes.labelOption}>
                 {t('navigationDrawer.editPage')}
               </Typography>
