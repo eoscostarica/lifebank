309aa6f56b36ad613b2a298bb17125539c0e8331
Edgar097 <edgar.parra@eoscostarica.io>
2021-05-07 20:22:39 -0600

    add(kubernetes files):  add k8s files and change env variables

--- utils/meta.mk
+++ utils/meta.mk
@@ -1,6 +1,6 @@
 -include .env
 
-VERSION=$(shell git rev-parse --short HEAD)
+VERSION ?= $(shell git rev-parse --short HEAD)
 CURRENT_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD)
 
 IMAGE_NAME_WEBAPP=lifebank-webapp
