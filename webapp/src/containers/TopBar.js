228ddb4592cf6c0c6b91cd855440fea6b358b991
Edgar097 <edgar.parra@eoscostarica.io>
2021-05-14 14:22:22 -0600

    feat(settings): first approach

--- webapp/src/containers/TopBar.js
+++ webapp/src/containers/TopBar.js
@@ -9,7 +9,7 @@ import { Link, useLocation, useHistory } from 'react-router-dom'
 import Divider from '@material-ui/core/Divider'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
-import ShareIcon from '@material-ui/icons/Share'
+import SettingsIcon from '@material-ui/icons/Settings';
 import clsx from 'clsx'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
@@ -90,21 +90,11 @@ const Topbar = ({ user, onLogout }) => {
   return (
     <Box className={classes.box}>
       <LanguageSelector />
-      {user && isDesktop && (
-        <IconButton onClick={handleClick}>
-          <ShareIcon
-            alt="User icon"
-            className={clsx(classes.userIcon, {
-              [classes.userIconTransparent]: useTransparentBG
-            })}
-          />
-        </IconButton>
-      )}
       {user && <Notification />}
       {user && (
         <>
           <IconButton onClick={handleClick}>
-            <PersonIcon
+            <SettingsIcon
               alt="User icon"
               className={clsx(classes.userIcon, {
                 [classes.userIconTransparent]: useTransparentBG
@@ -119,7 +109,10 @@ const Topbar = ({ user, onLogout }) => {
             onClose={handleClose}
           >
             <Link to="/profile" className={classes.link}>
-              <MenuItem className={classes.menuItem} onClick={handleClose}>{user.account}</MenuItem>
+              <MenuItem onClick={handleClose} className={classes.menuItem}>
+                <PersonIcon alt="User icon" className={classes.logoutIcon} />
+                {user.account}
+              </MenuItem>
             </Link>
             <Divider />
             <MenuItem onClick={handleLogout} className={classes.menuItem}>
@@ -128,9 +121,10 @@ const Topbar = ({ user, onLogout }) => {
             </MenuItem>
           </Menu>
         </>
-      )}
+      )
+      }
       <LoginModal isNavBar />
-    </Box>
+    </Box >
   )
 }
 
