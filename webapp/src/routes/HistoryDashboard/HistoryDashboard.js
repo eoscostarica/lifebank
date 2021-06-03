4b9b5b336e8de4f2a62bed7cf9afa13a1c353c0a
steph222 <stephdellbre@gmail.com>
2021-06-03 13:25:43 -0600

    feat(frontend): add selector

--- webapp/src/routes/HistoryDashboard/HistoryDashboard.js
+++ webapp/src/routes/HistoryDashboard/HistoryDashboard.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { makeStyles, useTheme } from '@material-ui/styles'
 import { useTranslation } from 'react-i18next'
 import { useLazyQuery } from '@apollo/react-hooks'
-import Typography from '@material-ui/core/Typography'
-import { GET_REPORT_QUERY, PROFILE_QUERY } from '../../gql'
 import MUIDataTable from 'mui-datatables'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+import InputLabel from '@material-ui/core/InputLabel'
 
+import { GET_REPORT_QUERY, PROFILE_QUERY } from '../../gql'
 
 import { useUser } from '../../context/user.context'
 import styles from './styles'
@@ -13,13 +16,18 @@ import { Box } from '@material-ui/core'
 
 const useStyles = makeStyles(styles)
 
-
 const HistoryDashboard = (user) => {
   const { t } = useTranslation('translations')
   const classes = useStyles()
   const [bodyReceive, setBodyReceive] = useState()
   const [bodySent, setBodySent] = useState()
   const [currentUser] = useUser()
+  const [optionSR, setOption] = useState('sent')
+
+  const options = [
+    { value: 'sent', label: t('historyDashboard.tableTitleSent') },
+    { value: 'received', label: t('historyDashboard.tableTitleReceived') }
+  ]
 
   const [
     loadProfile,
@@ -53,95 +61,136 @@ const HistoryDashboard = (user) => {
 
   }
 
+  const handleChange = (event) => {
+    setOption(event.target.value);
+  };
+
   return (
     <>
-      <Box>
-        {bodySent && bodySent.length > 0 &&
-          <MUIDataTable
-            title={t('historyDashboard.tableTitleSent')}
-            data={bodySent.map((notification) => [
-              notification.send_to,
-              notification.created_at_date,
-              notification.created_at_time,
-              notification.tokens
-            ])}
-            columns={[
-              {
-                name: t('historyDashboard.donor'),
-                options: {
-                  filter: true,
-                }
-              },
-              {
-                name: t('historyDashboard.date'),
-                options: {
-                  filter: true,
-                }
-              },
-              {
-                name: t('historyDashboard.time'),
-                options: {
-                  filter: true,
-                }
-              },
-              {
-                name: t('historyDashboard.tokens'),
-                options: {
-                  filter: true,
-                }
+
+      <Box >
+        <Box className={classes.boxSelect}>
+          <FormControl variant="filled" className={classes.formControl}>
+            <InputLabel id="demo-simple-select-outlined-label">{t('historyDashboard.dashboard')}</InputLabel>
+            <Select
+              labelId="demo-simple-select-filled-label"
+              id="demo-simple-select-filled"
+              value={optionSR}
+              onChange={handleChange}
+              label="optionSR"
+            >
+              {options.map((option) => {
+                return (
+                  <MenuItem
+                    key={option.value}
+                    value={option.value}
+                  >
+                    {option.label}
+                  </MenuItem>
+                )
+              })}
+            </Select>
+          </FormControl>
+        </Box>
+        <Box className={classes.root}>
+          {optionSR === 'sent' && (
+            <Box>
+              {bodySent && bodySent.length > 0 &&
+
+                <Box className={classes.tableContent} >
+                  <MUIDataTable
+                    title={t('historyDashboard.tableTitleSent')}
+                    data={bodySent.map((notification) => [
+                      notification.send_to,
+                      notification.created_at_date,
+                      notification.created_at_time,
+                      notification.tokens
+                    ])}
+                    columns={[
+                      {
+                        name: t('historyDashboard.donor'),
+                        options: {
+                          filter: true,
+                        }
+                      },
+                      {
+                        name: t('historyDashboard.date'),
+                        options: {
+                          filter: true,
+                        }
+                      },
+                      {
+                        name: t('historyDashboard.time'),
+                        options: {
+                          filter: true,
+                        }
+                      },
+                      {
+                        name: t('historyDashboard.tokens'),
+                        options: {
+                          filter: true,
+                        }
+                      }
+                    ]}
+                    options={{
+                      print: false,
+                      selectableRowsHideCheckboxes: true,
+                      selectableRowsHeader: false,
+                      download: false,
+                    }}
+                  />
+                </Box>
               }
-            ]}
-            options={{
-              print: false,
-              selectableRowsHideCheckboxes: true,
-              selectableRowsHeader: false,
-              download: false,
-            }}
-          />
-        }
-        {bodyReceive && bodyReceive.length > 0 &&
-          <MUIDataTable
-            title={t('historyDashboard.tableTitleSent')}
-            data={bodyReceive.map((notification) => [
-              notification.send_to,
-              notification.created_at_date,
-              notification.created_at_time,
-              notification.tokens
-            ])}
-            columns={[
-              {
-                name: t('historyDashboard.donor'),
-                options: {
-                  filter: true,
-                }
-              },
-              {
-                name: t('historyDashboard.date'),
-                options: {
-                  filter: true,
-                }
-              },
-              {
-                name: t('historyDashboard.time'),
-                options: {
-                  filter: true,
-                }
-              },
-              {
-                name: t('historyDashboard.tokens'),
-                options: {
-                  filter: true,
-                }
+            </Box>)}
+          {optionSR === 'received' && (
+            <Box>
+              {bodyReceive && bodyReceive.length > 0 &&
+                <Box className={classes.tableContent} >
+                  <MUIDataTable
+                    title={t('historyDashboard.tableTitleSent')}
+                    data={bodyReceive.map((notification) => [
+                      notification.send_to,
+                      notification.created_at_date,
+                      notification.created_at_time,
+                      notification.tokens
+                    ])}
+                    columns={[
+                      {
+                        name: t('historyDashboard.donor'),
+                        options: {
+                          filter: true,
+                        }
+                      },
+                      {
+                        name: t('historyDashboard.date'),
+                        options: {
+                          filter: true,
+                        }
+                      },
+                      {
+                        name: t('historyDashboard.time'),
+                        options: {
+                          filter: true,
+                        }
+                      },
+                      {
+                        name: t('historyDashboard.tokens'),
+                        options: {
+                          filter: true,
+                        }
+                      }
+                    ]}
+                    options={{
+                      print: false,
+                      selectableRowsHideCheckboxes: true,
+                      selectableRowsHeader: false,
+                      download: false,
+                    }}
+                  />
+                </Box>
               }
-            ]}
-            options={{
-              print: false,
-              selectableRowsHideCheckboxes: true,
-              selectableRowsHeader: false,
-              download: false,
-            }}
-          />
-        }
+            </Box>)}
+        </Box>
       </Box>
     </>
   )
--- webapp/src/routes/HistoryDashboard/styles.js
+++ webapp/src/routes/HistoryDashboard/styles.js
@@ -11,5 +11,31 @@ export default (theme) => ({
       paddingLeft: '5%',
       paddingRight: '5%'
     },
-  }
+  },
+  boxSelect: {
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'row',
+    [theme.breakpoints.down('md')]: {
+      justifyContent: 'center'
+    }
+  },
+  formControl: {
+    margin: theme.spacing(2),
+    marginLeft: 25,
+    minWidth: 180
+  },
+  root: {
+    padding: theme.spacing(4),
+    display: 'flex',
+    height: 'calc(100vh - 60px)',
+    width: "90%",
+    [theme.breakpoints.down('md')]: {
+      width: "100%",
+    }
+  },
+  tableContent: {
+    paddingTop: "30px",
+    width: "100%"
+  },
 })
