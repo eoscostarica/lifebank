ab9c515a9f545a34b5cb52ba5fb020433e06cea5
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2021-06-04 14:57:04 -0600

    feat(backend): solve if logic conflict

--- hapi/src/api/account.api.js
+++ hapi/src/api/account.api.js
@@ -492,7 +492,7 @@ const getReport = async (where, account) => {
 
   if (!user) throw new Error('No valid account')
 
-  if (user.role === 'sponsor')
+  if (user.role === 'donor')
     return await getReportDonor(where, user.account)
   else if (user.role === 'sponsor')
     return await getReportSponsor(where, user.account)
