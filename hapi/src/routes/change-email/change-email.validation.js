686f960748eb46c913d4516521ef1c2d5ba679a4
Edgar Parra <76976401+Edgar097@users.noreply.github.com>
2021-06-06 11:20:04 -0600

    feat(subscription and change email) (#783)
    * feat(subscription and change email)
    
    * fix(topbar)
    
    * feat(change email) backend
    
    * merge master
    
    * fix(change email) settings
    
    * fix(settings): add functionality

---
+++ hapi/src/routes/change-email/change-email.validation.js
@@ -0,0 +1,10 @@
+const Joi = require('@hapi/joi')
+
+module.exports = {
+  payload: Joi.object({
+    input: Joi.object({
+      account: Joi.string().required(),
+      email: Joi.string().required()
+    })
+  }).options({ stripUnknown: true })
+}
