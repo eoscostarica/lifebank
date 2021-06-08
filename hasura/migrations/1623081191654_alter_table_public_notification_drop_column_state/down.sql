dcce5aa3b403da4c617522d8efc97f68c82b086a
steph222 <stephdellbre@gmail.com>
2021-06-08 17:19:45 -0600

    fix(frontend): deleted migrations

--- hasura/migrations/1623081191654_alter_table_public_notification_drop_column_state/down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "public"."notification" ADD COLUMN "state" bool;
-ALTER TABLE "public"."notification" ALTER COLUMN "state" DROP NOT NULL;
-ALTER TABLE "public"."notification" ALTER COLUMN "state" SET DEFAULT false;
