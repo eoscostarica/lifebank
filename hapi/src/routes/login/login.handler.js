8ced97c790648a821f036bb6099b8bb0742c3d40
codefactor-io <support@codefactor.io>
2021-06-28 23:18:57 +0000

    [CodeFactor] Apply fixes

--- hapi/src/routes/login/login.handler.js
+++ hapi/src/routes/login/login.handler.js
@@ -3,7 +3,6 @@ const { BAD_REQUEST } = require('http-status-codes')
 
 const { accountApi } = require('../../api')
 
-
 module.exports = async ({ payload: { input } }) => {
   try {
     const response = await accountApi.login(input)
--- hapi/src/twitterBot/statusesUpdate.js
+++ hapi/src/twitterBot/statusesUpdate.js
@@ -6,44 +6,44 @@ const request = require('request').defaults({ encoding: null })
 const Twit = require('twit')
 
 const tweet = (message, file) => {
-  var T = new Twit( config )
-  var localname = `tempFile${Date.now()}`;
-  var PATH = path.join(
-    os.tmpdir(),
-    `./${localname}`
-  );
-  var mediaUrl = file
-  var responseReturn = {}
-  var metadataData = {}
+  const T = new Twit(config)
+  const localname = `tempFile${Date.now()}`
+  const PATH = path.join(os.tmpdir(), `./${localname}`)
+  const mediaUrl = file
+  let responseReturn = {}
+  let metadataData = {}
   request.get(mediaUrl, function (error, response, body) {
     responseReturn = Object.assign(response)
     if (error) console.error(error)
-    fs.writeFile(PATH, body, function(error) {
+    fs.writeFile(PATH, body, function (error) {
       // step ONE
       if (error) console.error(error)
       T.postMediaChunked({ file_path: PATH }, function (error, data, response) {
         if (error) console.error(error)
         responseReturn = Object.assign(response)
-        const mediaIdStr = data.media_id_string;
-        const meta_params = { media_id: mediaIdStr };
+        const mediaIdStr = data.media_id_string
+        const meta_params = { media_id: mediaIdStr }
         //  step TWO
-        T.post('media/metadata/create', meta_params, function (error, data, response) {
-          metadataData = data
-          responseReturn = Object.assign(response)
-          if (!error) {
-            const params = { status: message, media_ids: [mediaIdStr] };
-            // step THREE
-            T.post('statuses/update', params, function (error, response) {
-              responseReturn = Object.assign(response)
-              if (error) console.error(error)
-              fs.unlinkSync(PATH);
-              return responseReturn
-            })
+        T.post(
+          'media/metadata/create',
+          meta_params,
+          function (error, data, response) {
+            metadataData = data
+            responseReturn = Object.assign(response)
+            if (!error) {
+              const params = { status: message, media_ids: [mediaIdStr] }
+              // step THREE
+              T.post('statuses/update', params, function (error, response) {
+                responseReturn = Object.assign(response)
+                if (error) console.error(error)
+                fs.unlinkSync(PATH)
+                return responseReturn
+              })
+            } else console.error(error)
           }
-          else console.error(error)
-        }) 
+        )
       })
-    }) 
-  }) 
+    })
+  })
 }
 module.exports = { tweet }
